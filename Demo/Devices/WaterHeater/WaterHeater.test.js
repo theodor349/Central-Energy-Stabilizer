bf11a517c6c289eb3d6f515ae1b3acdb5e3b072c
Theodor Risager <theodor349@gmail.com>
2020-03-09 14:13:40 +0100

    Trying to get Jest Working

---
+++ Demo/Devices/WaterHeater/WaterHeater.test.js
@@ -0,0 +1,13 @@
+const waterHeater = require('./WaterHeater.js');
+
+test("Test", () => {
+  let heater = {
+    currentTemp: 55, // Current temperature
+    lowerLimit: 55, // When under it should start
+    upperLimit: 85, // When abow it turns off
+    effect: 1000, // Watts
+    state: 1, // 0 = off, 1 = standby, 2 = full power
+  }
+  waterHeater.update(heater);
+  expect(heater.effect).toStrictEqual(2);
+});

de6c7eae0d0ed27561de4f3b580b88e306c3e0e1
todes92 <todeszeichen@hotmail.com>
2020-03-10 10:33:43 +0100

    added checkTemp tests to WaterHeater.test.js

--- Demo/Devices/WaterHeater/WaterHeater.test.js
+++ Demo/Devices/WaterHeater/WaterHeater.test.js
@@ -1,6 +1,6 @@
 const waterHeater = require('./WaterHeater.js');
 
-test("Test", () => {
+test("Test: example test", () => {
   let heater = {
     currentTemp: 55, // Current temperature
     lowerLimit: 55,  // When under it should start
@@ -13,7 +13,7 @@ test("Test", () => {
 });
 
 //Test for update function
-test("Testing the update function", () => {
+test("Testing the update function (effect 1000) (success)", () => {
 	let heater = {
 		currentTemp: 55, // Current temperature
 		lowerLimit: 55,  // When under it should start
@@ -25,7 +25,7 @@ test("Testing the update function", () => {
 	expect(heater.effect).toBe(1000);
 });
 
-test("Testing the update function", () => {
+test("Testing the update function (effect 1000) (failure)", () => {
   let heater = {
     currentTemp: 55, // Current temperature
     lowerLimit: 55,  // When under it should start
@@ -37,8 +37,8 @@ test("Testing the update function", () => {
   expect(heater.effect).not.toEqual(2);
 });
 
-//Test for the updateTemp function
-test("Testing the updateTemp function", () => {
+//Test for the updateTemp function state temp change
+test("Testing the updateTemp function with state: standby (success)", () => {
 	let heater = {
 		currentTemp: 55, // Current temperature
 		lowerLimit: 55,  // When under it should start
@@ -50,7 +50,7 @@ test("Testing the updateTemp function", () => {
 	expect(heater.currentTemp).toBe(55);
 });
 
-test("Testing the updateTemp function", () => {
+test("Testing the updateTemp function with state: standby (failure)", () => {
 	let heater = {
 		currentTemp: 55, // Current temperature
 		lowerLimit: 55,  // When under it should start
@@ -62,7 +62,7 @@ test("Testing the updateTemp function", () => {
 	expect(heater.currentTemp).not.toBe(50);
 });
 
-test("Testing the updateTemp function", () => {
+test("Testing the updateTemp function with state: full power (success)", () => {
 	let heater = {
 		currentTemp: 55, // Current temperature
 		lowerLimit: 55,  // When under it should start
@@ -74,7 +74,7 @@ test("Testing the updateTemp function", () => {
 	expect(heater.currentTemp).toBe(56);
 });
 
-test("Testing the updateTemp function", () => {
+test("Testing the updateTemp function with state: full power (failure)", () => {
 	let heater = {
 		currentTemp: 55, // Current temperature
 		lowerLimit: 55,  // When under it should start
@@ -86,7 +86,7 @@ test("Testing the updateTemp function", () => {
 	expect(heater.currentTemp).not.toBe(55);
 });
 
-test("Testing the updateTemp function", () => {
+test("Testing the updateTemp function with state: off (success)", () => {
 	let heater = {
 		currentTemp: 55, // Current temperature
 		lowerLimit: 55,  // When under it should start
@@ -97,7 +97,7 @@ test("Testing the updateTemp function", () => {
 	waterHeater.updateTemp(heater);
 	expect(heater.currentTemp).toBe(54);
 });
-test("Testing the updateTemp function", () => {
+test("Testing the updateTemp function with state: off (failure)", () => {
 	let heater = {
 		currentTemp: 55, // Current temperature
 		lowerLimit: 55,  // When under it should start
@@ -109,7 +109,128 @@ test("Testing the updateTemp function", () => {
 	expect(heater.currentTemp).not.toBe(55);
 });
 
-//Test for the updateState function
+//Test for state in temperature outside of limits
+test("Testing the checkTemp function with currentTemp: 55 (success)", () => {
+	let heater = {
+		currentTemp: 55, // Current temperature
+		lowerLimit: 55,  // When under it should start
+		upperLimit: 85,  // When above it turns off
+		effect: 1000,    // Watts
+		state: 1,        // 0 = off, 1 = standby, 2 = full power
+	}
+	waterHeater.checkTemp(heater);
+	expect(heater.state).toBe(1);
+});
+
+test("Testing the checkTemp function with currentTemp: 55 (failure)", () => {
+	let heater = {
+		currentTemp: 55, // Current temperature
+		lowerLimit: 55,  // When under it should start
+		upperLimit: 85,  // When above it turns off
+		effect: 1000,    // Watts
+		state: 1,        // 0 = off, 1 = standby, 2 = full power
+	}
+	waterHeater.checkTemp(heater);
+	expect(heater.state).not.toBe(0);
+});
+
+test("Testing the checkTemp function with currentTemp: 54 (success)", () => {
+	let heater = {
+		currentTemp: 54, // Current temperature
+		lowerLimit: 55,  // When under it should start
+		upperLimit: 85,  // When above it turns off
+		effect: 1000,    // Watts
+		state: 1,        // 0 = off, 1 = standby, 2 = full power
+	}
+	waterHeater.checkTemp(heater);
+	expect(heater.state).toBe(2);
+});
+
+test("Testing the checkTemp function with currentTemp: 54 (failure)", () => {
+	let heater = {
+		currentTemp: 54, // Current temperature
+		lowerLimit: 55,  // When under it should start
+		upperLimit: 85,  // When above it turns off
+		effect: 1000,    // Watts
+		state: 1,        // 0 = off, 1 = standby, 2 = full power
+	}
+	waterHeater.checkTemp(heater);
+	expect(heater.state).not.toBe(1);
+});
+
+test("Testing the checkTemp function with currentTemp: 20 (success)", () => {
+	let heater = {
+		currentTemp: 20, // Current temperature
+		lowerLimit: 55,  // When under it should start
+		upperLimit: 85,  // When above it turns off
+		effect: 1000,    // Watts
+		state: 1,        // 0 = off, 1 = standby, 2 = full power
+	}
+	waterHeater.checkTemp(heater);
+	expect(heater.state).toBe(2);
+});
+
+test("Testing the checkTemp function with currentTemp: 20 (failure)", () => {
+	let heater = {
+		currentTemp: 20, // Current temperature
+		lowerLimit: 55,  // When under it should start
+		upperLimit: 85,  // When above it turns off
+		effect: 1000,    // Watts
+		state: 1,        // 0 = off, 1 = standby, 2 = full power
+	}
+	waterHeater.checkTemp(heater);
+	expect(heater.state).not.toBe(1);
+});
+
+test("Testing the checkTemp function with currentTemp: 86 (success)", () => {
+	let heater = {
+		currentTemp: 86, // Current temperature
+		lowerLimit: 55,  // When under it should start
+		upperLimit: 85,  // When above it turns off
+		effect: 1000,    // Watts
+		state: 1,        // 0 = off, 1 = standby, 2 = full power
+	}
+	waterHeater.checkTemp(heater);
+	expect(heater.state).toBe(0);
+});
+
+test("Testing the checkTemp function with state: 86 (failure)", () => {
+	let heater = {
+		currentTemp: 86, // Current temperature
+		lowerLimit: 55,  // When under it should start
+		upperLimit: 85,  // When above it turns off
+		effect: 1000,    // Watts
+		state: 1,        // 0 = off, 1 = standby, 2 = full power
+	}
+	waterHeater.checkTemp(heater);
+	expect(heater.state).not.toBe(2);
+});
+
+test("Testing the checkTemp function with currentTemp: 100 (success)", () => {
+	let heater = {
+		currentTemp: 100, // Current temperature
+		lowerLimit: 55,  // When under it should start
+		upperLimit: 85,  // When above it turns off
+		effect: 1000,    // Watts
+		state: 1,        // 0 = off, 1 = standby, 2 = full power
+	}
+	waterHeater.checkTemp(heater);
+	expect(heater.state).toBe(0);
+});
+
+test("Testing the checkTemp function with state: 100 (failure)", () => {
+	let heater = {
+		currentTemp: 100, // Current temperature
+		lowerLimit: 55,  // When under it should start
+		upperLimit: 85,  // When above it turns off
+		effect: 1000,    // Watts
+		state: 1,        // 0 = off, 1 = standby, 2 = full power
+	}
+	waterHeater.checkTemp(heater);
+	expect(heater.state).not.toBe(2);
+});
+
+//Test for the updateState function for changing state
 test("Testing the updateState function", () => {
 	let heater = {
 		currentTemp: 55, // Current temperature
@@ -133,6 +254,7 @@ test("Testing the updateState function", () => {
 	waterHeater.updateState(heater, 2);
 	expect(heater.state).not.toBe(1);
 });
+
 test("Testing the updateState function", () => {
 	let heater = {
 		currentTemp: 55, // Current temperature
@@ -156,6 +278,7 @@ test("Testing the updateState function", () => {
 	waterHeater.updateState(heater, 2);
 	expect(heater.state).not.toBe(1);
 });
+
 test("Testing the updateState function", () => {
 	let heater = {
 		currentTemp: 55, // Current temperature

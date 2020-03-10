dd52b34aa557b73862cac6a9ff8588c2b88806fa
Theodor Risager <theodor349@gmail.com>
2020-03-10 14:39:02 +0100

    Made Device manager

--- Demo/Server/DeviceManager/deviceManager.js
+++ Demo/Server/DeviceManager/deviceManager.js
@@ -1,24 +1,59 @@
 const functions = {
-  add: (device) => add(device),
-  remove: (device) => remove(device),
-  schedule: (devices) => schedule(devices),
-  doActions: (devices) => doActions(devices),
+  add: (waterHeater) => add(waterHeater),
+  remove: (waterHeater) => remove(waterHeater),
+  schedule: (waterHeater) => schedule(waterHeater),
+  doActions: (waterHeater) => doActions(waterHeater),
 }
 
-function add(device) {
-
+let nextId = 0;
+let testerWaterHeater = {
+  id: NaN, // ID
+  currentTemp: 55, // Current temperature
+  lowerLimit: 55, // When under it should start
+  upperLimit: 85, // When abow it turns off
+  effect: 1000, // Watts
+  state: 1, // 0 = off, 1 = standby, 2 = full power
+  timeOn: undefined,
+  timeOff: undefined,
 }
 
-function remove(device) {
+let waterHeater = undefined;
 
+
+
+function add(waterHeater) {
+  waterHeater.id = nextId;
+  nextId++;
+  waterHeater = waterHeater;
 }
 
-function schedule(devices) {
-
+function remove(waterHeater) {
+  waterHeater = undefined;
 }
 
-function doActions(devices) {
+function update(waterHeater) {
+  if (waterHeater.timeOn == undefined && waterHeater.timeOff == undefined)
+    schedule(waterHeater);
+  doActions(waterHeater);
+}
 
+function schedule(waterHeater) {
+  let date = new Date();
+  date.setMinutes(date.getMinutes() + 1);
+  waterHeater.timeOn = date;
+
+  date.setMinutes(date.getMinutes() + 2);
+}
+
+function doActions(waterHeater) {
+  let date = new Date();
+  if (waterHeater.timeOn != undefined) {
+    if (waterHeater.timeOn >= date)
+      console.log("Turn on");
+  } else if (waterHeater.timeOff != undefined) {
+    if (waterHeater.timeOff >= date)
+      console.log("Turn off");
+  }
 }
 
 

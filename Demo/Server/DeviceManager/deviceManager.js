1a2e70ac8a93fe911c223ab5725109ebc68c1629
Theodor Risager <theodor349@gmail.com>
2020-03-10 12:46:04 +0100

    Update deviceManager.js

--- Demo/Server/DeviceManager/deviceManager.js
+++ Demo/Server/DeviceManager/deviceManager.js
@@ -1,4 +1,23 @@
 const functions = {
+  add: (device) => add(device),
+  remove: (device) => remove(device),
+  schedule: (devices) => schedule(devices),
+  doActions: (devices) => doActions(devices),
+}
+
+function add(device) {
+
+}
+
+function remove(device) {
+
+}
+
+function schedule(devices) {
+
+}
+
+function doActions(devices) {
 
 }
 

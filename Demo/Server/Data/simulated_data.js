0f3162a2967b812c70b55ee1adced0b35ca409b9
EmilGHenriksen <56338371+EmilGHenriksen@users.noreply.github.com>
2020-03-12 12:58:09 +0100

    Update simulated_data.js

--- Demo/Server/Data/simulated_data.js
+++ Demo/Server/Data/simulated_data.js
@@ -1,5 +1,5 @@
 function sim_data(time) {
-	return (15*(Math.sin(3*time)*Math.cos(12*time))) + 15;
+	return (15*(Math.sin((3*time)/100)*Math.cos((12*time)/100))) + 15;
 }
 
 module.exports = sim_data;

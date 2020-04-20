bd58d812ea3413085e1dc58a76dd6678eaa9cec6
Theodor Risager <theodor349@gmail.com>
2020-04-20 13:55:16 +0200

    Testing moved to Test folder

--- Implementation/Server/Test/DbAccessor.test.js
+++ Implementation/Server/Test/DbAccessor.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const da = require('./DatabaseAccessor.js');
+const da = require('./../DatabaseAccessor.js');
 
 function createDevicePrototype() {
     let program_0 = {

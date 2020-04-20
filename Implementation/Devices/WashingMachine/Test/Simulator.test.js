c95b7ece5df5b4d56eadd724bd1b2b598884efce
asa19aau <asa19aau@student.aau.dk>
2020-04-20 14:24:05 +0200

    added test files to devices

---
+++ Implementation/Devices/WashingMachine/Test/Simulator.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const app = require('./../App.js');
+
+describe('', () =>
+	{
+		it('', () =>
+			{
+                assert();
+			});
+	});
---
+++ Implementation/Devices/WaterHeater/Test/Simulation.test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+const app = require('./../App.js');
+
+describe('', () =>
+	{
+		it('', () => {
+			assert();
+		});
+	});

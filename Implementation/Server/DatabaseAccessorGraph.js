4167fcfb2232b211127e2b4475154f26951c61e2
Theodor Risager <theodor349@gmail.com>
2020-04-23 10:09:25 +0200

    Inserted some Comments

--- Implementation/Server/DatabaseAccessorGraph.js
+++ Implementation/Server/DatabaseAccessorGraph.js
@@ -16,18 +16,12 @@ async function run(connectionString) {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
-    //test();
 }
 
 async function dropDatabase() {
     // Clear the database every time. This is for the sake of example only,
     // don't do this in prod :)
     await mongoose.connection.dropDatabase();
-}
-
-// TODO: Remove tests
-function test() {
-    dropDatabase();
 }
 
 /*
@@ -111,11 +105,13 @@ async function updateGraph(id, points, shouldAdd) {
     return new Promise(async (resolve, reject) => {
         getGraphHelper(id)
             .then((graph) => {
+                // Check
                 if (graph === null)
                     resolve(false);
 
-                let values = JSON.parse(graph.values);
 
+                // Update Values
+                let values = JSON.parse(graph.values);
                 if (shouldAdd) {
                     for (let i = 0; i < points.length; i++) {
                         if (points[i] === 'n')
@@ -130,6 +126,7 @@ async function updateGraph(id, points, shouldAdd) {
                     }
                 }
 
+                // Set update variables
                 let conditions = {
                     graphId: id
                 };
@@ -138,6 +135,7 @@ async function updateGraph(id, points, shouldAdd) {
                 };
                 let options = {};
 
+                // Update
                 Graph.updateOne(conditions, update, options, (err, success) => {
                     if (err)
                         reject(err);
@@ -177,27 +175,4 @@ function deserilizeGraph(graph) {
 
 function validUpdate(values) {
     return values.length === 60;
-}
-
-/*
-    TODO: Remove
-          For testing only
-*/
-
-// Graphs
-function createGraphPrototype() {
-    let values = [
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-        10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-        20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-        30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-        40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
-        50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
-    ];
-
-    let graph = {
-        graphId: "id",
-        values: values
-    };
-    return graph;
 }

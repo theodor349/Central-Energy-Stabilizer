a05d80ba248d2aba3e0ec593fc0b5461bf097095
EmilGHenriksen <56338371+EmilGHenriksen@users.noreply.github.com>
2020-04-28 15:33:59 +0200

    Device manager update

--- Implementation/Server/DeviceManager.js
+++ Implementation/Server/DeviceManager.js
@@ -1,3 +1,4 @@
+'use strict';
 const functions = {
     onConnect: (socket) => onConnect(socket),
     receiveId: (id) => receiveId(id),
@@ -11,5 +12,19 @@ const functions = {
     stateChanged: (id, newState) => stateChanged(id, newState),
     receiveUpdate: (deviceInfo) => receiveUpdate(deviceInfo),
     removeSchedule: (id) => removeSchedule(id),
+    getCommandQueue: () => getCommandQueue(),
 }
 module.exports = functions;
+let commandQueue = [];
+
+function getCommandQueue() {
+    return commandQueue;
+}
+
+function createCommand(command, payload) {
+    let commandObj = {
+        command: command,
+        payload: payload,
+    }
+    commandQueue.push(commandObj);
+}

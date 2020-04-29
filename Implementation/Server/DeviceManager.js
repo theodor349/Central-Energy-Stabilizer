19600b114f9693fea11cfbc1f753de1bb2806d79
Theodor Risager <theodor349@gmail.com>
2020-04-29 13:36:04 +0200

    Device Mananger 1st and 2nd function

--- Implementation/Server/DeviceManager.js
+++ Implementation/Server/DeviceManager.js
@@ -1,8 +1,10 @@
 'use strict';
+const uuid = require('uuidv4');
+
 const functions = {
     onConnect: (socket) => onConnect(socket),
-    receiveId: (id) => receiveId(id),
-    deviceInit: (deviceInfo) => deviceInit(deviceInfo),
+    receiveId: (id, socket) => receiveId(id, socket),
+    deviceInit: (deviceInfo, socket) => deviceInit(deviceInfo, socket),
     onDisconnect: (socket) => onDisconnect(socket),
     deleteDevice: (id) => deleteDevice(id),
     updateDevice: (id) => updateDevice(id),
@@ -16,15 +18,54 @@ const functions = {
 }
 module.exports = functions;
 let commandQueue = [];
+let activeConnections = [];
+
+function onConnect(socket) {
+    createCommand(socket, "askForId");
+}
+
+function receiveId(id, socket) {
+    if (uuid.isUuid(id) === true) {
+        addConnection(id, socket);
+        return true;
+    } else {
+        sendNewId(socket);
+        return false;
+    }
+}
+
+/*
+    SECTION: Helper Functions
+*/
+
+function sendNewId(socket) {
+    let id = uuid.uuid();
+    createCommand(socket, "setId", id);
+}
+
+function addConnection(id, socket) {
+    let connection = {
+        deviceId: id,
+        socket: socket
+    };
+    activeConnections.push(connection);
+}
+
+function isIdValid(id) {
+    if (id === undefined) {
+        return false;
+    }
+}
 
 function getCommandQueue() {
     let cq = commandQueue;
-    commandQueue.clear();
+    commandQueue = [];
     return cq;
 }
 
-function createCommand(command, payload) {
+function createCommand(socket, command, payload) {
     let commandObj = {
+        socket: socket,
         command: command,
         payload: payload,
     }
--- Implementation/Server/Test/DeviceManager.test.js
+++ Implementation/Server/Test/DeviceManager.test.js
@@ -1,14 +1,51 @@
 const assert = require('assert');
 const dm = require('./../DeviceManager.js');
 const db = require('./../DatabaseAccessorDevice.js');
+const uuid = require('uuidv4');
 
-describe('Device Manager', () => {
+if (true) {
+    describe('Device Manager', () => {
 
-    it('onConnect: Adds command to commandQueue', async() => {
-        dm.onConnect("socket");
-        let commandQueue = dm.getCommandQueue();
-        assert(commandQueue !== undefined &&
-            commandQueue.length === 1 &&
-            commandQueue[0].command === 'askForId');
+        // On Connect
+        it('onConnect: Adds command to commandQueue', async () => {
+            dm.onConnect("socket");
+            let commandQueue = dm.getCommandQueue();
+            assert(commandQueue !== undefined &&
+                commandQueue.length === 1 &&
+                commandQueue[0].socket === "socket" &&
+                commandQueue[0].command === 'askForId');
+        })
+
+        // Receive ID
+        it('receiveId: Connection with good uuId', async () => {
+            let res = dm.receiveId("3d71d761-72ea-4955-ae8c-e7700099e41a", "socket");
+            let commandQueue = dm.getCommandQueue();
+            assert(res === true &&
+                commandQueue !== undefined &&
+                commandQueue.length === 0);
+        })
+        it('receiveId: Connection with wrong uuId', async () => {
+            let res = dm.receiveId("PLZ return false", "socket");
+            let commandQueue = dm.getCommandQueue();
+            assert(res === false &&
+                commandQueue !== undefined &&
+                commandQueue.length === 1 &&
+                commandQueue[0].socket === "socket" &&
+                commandQueue[0].command === 'setId' &&
+                uuid.isUuid(commandQueue[0].payload));
+        })
+        it('receiveId: Connection without Id', async () => {
+            let id;
+            let res = dm.receiveId(id, "socket");
+            let commandQueue = dm.getCommandQueue();
+            assert(res === false &&
+                commandQueue !== undefined &&
+                commandQueue.length === 1 &&
+                commandQueue[0].socket === "socket" &&
+                commandQueue[0].command === 'setId' &&
+                uuid.isUuid(commandQueue[0].payload));
+        })
+
+        // Device initTemp
     })
-})
+}

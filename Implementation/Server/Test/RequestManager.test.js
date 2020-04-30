6fc41a37f4605951f3db27d91e715126597e8000
Sarah F <sfrand19@student.aau.dk>
2020-04-30 14:35:42 +0200

    Minor mistake fixed from last commit

--- Implementation/Server/Test/RequestManager.test.js
+++ Implementation/Server/Test/RequestManager.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const rqstMngr = require('./../RequestManager.js');
 const da = require('./../DatabaseAccessorDevice.js');
+const forecaster = require('./../Forecaster.js');
 
 if (true) {
     describe('Request Manager', () => {
@@ -32,5 +33,33 @@ if (true) {
                 timeIntervalFinish: new Date(2010, 0, 24, 18, 30)
                 };
         });
-    });
+
+        /*
+        it('requestTimeToRun: isScheduled === true', async() => {
+            let graph = [21, 13, 22, 29, 35, 10, 17, 20, 39, 37,
+                        21, 13, 22, 29, 35, 10, 17, 20, 39, 37,
+                        21, 13, 22, 29, 35, 10, 17, 20, 39, 37,
+                        21, 13, 22, 29, 35, 10, 17, 20, 39, 37,
+                        21, 13, 22, 29, 35, 10, 17, 20, 39, 37,
+                        21, 13, 22, 29, 35, 10, 17, 20, 39, 37];
+            let isScheduled = true;
+            let timeIntervalObject = {
+                timeIntervalStart: new Date(2010, 0, 24, 15, 30),
+                timeIntervalFinish: new Date(2010, 0, 24, 18, 30)
+                };
+            let currentSchedule = {
+                timeIntervalStart: new Date(2010, 0, 24, 15, 45),
+                timeIntervalFinish: new Date(2010, 0, 24, 16, 45)
+            };
+            let expectedFinishTime = new Date(2010, 0, 24, 16, 45);
+            
+            forecaster.addDemand(currentSchedule.timeIntervalStart, graph);
+            
+            let res = await rqstMngr.requestTimeToRun(graph, isScheduled, timeIntervalObject, currentSchedule);   
+
+            assert(res)
+
+
+        }); */
+    }); 
 }

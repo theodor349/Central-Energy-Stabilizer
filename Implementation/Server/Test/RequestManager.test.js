2bf642286767ddc1e834877817b587ba712421b8
Sarah F <sfrand19@student.aau.dk>
2020-04-30 14:32:30 +0200

    Request Manager test 1 done, working on test 2 and 3
    Test: "requestTimeToRun: expected output and not undefined output" works, assuming the functions from forecaster works correctly.
    
    Still working on tests 2 and 3.

--- Implementation/Server/Test/RequestManager.test.js
+++ Implementation/Server/Test/RequestManager.test.js
@@ -1,35 +1,36 @@
 const assert = require('assert');
 const rqstMngr = require('./../RequestManager.js');
 const da = require('./../DatabaseAccessorDevice.js');
-/*
 
 if (true) {
     describe('Request Manager', () => {
         it('requestTimeToRun: expected output and not undefined output', async() => {
             let graph = [21, 13, 22, 29];
-            let isScheduled = true;
+            let isScheduled = false;
             let timeIntervalObject = {
-                timeIntervalStart: new Date(2010, 1, 24, 15, 30),
-                timeIntervalFinish: new Date(2010, 1, 24, 18, 30)
+                timeIntervalStart: new Date(2010, 0, 24, 15, 30),
+                timeIntervalFinish: new Date(2010, 0, 24, 18, 30)
                 };
             // Change this once blackbox of Fit Demand Graph is done
-            let expectedFinishTime = new Date(2010, 1, 24, 15, 34);
+            let expectedFinishTime = new Date(2010, 0, 24, 15, 34);
+            let currentSchedule = undefined;
 
-            let res = rqstMngr.requestTimeToRun(graph, isScheduled, timeIntervalObject);
+            let res = await rqstMngr.requestTimeToRun(graph, isScheduled, timeIntervalObject, currentSchedule);
+
             assert(res.outputIntervalStart !== undefined &&
-                    res.outputIntervalStart === timeIntervalObject.timeIntervalStart &&
+                    res.outputIntervalStart.getTime() === timeIntervalObject.timeIntervalStart.getTime() &&
                     res.outputIntervalFinish !== undefined &&
-                    res.outputIntervalFinish === expectedFinishTime);
+                    res.outputIntervalFinish.getTime() === expectedFinishTime.getTime()
+                    );
         });
 
         it('gatherSurplusGraph: Testing if surplus graph is gathered', async() => {
             let graph = [21, 13, 22, 29];
             let isScheduled = true;
             let timeIntervalObject = {
-                timeIntervalStart: new Date(2010, 1, 24, 15, 30),
-                timeIntervalFinish: new Date(2010, 1, 24, 18, 30)
+                timeIntervalStart: new Date(2010, 0, 24, 15, 30),
+                timeIntervalFinish: new Date(2010, 0, 24, 18, 30)
                 };
         });
     });
 }
-*/
